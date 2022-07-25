566ba7a4f68896691ce6c1d090ab3798e0301298
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-07-25 14:25:01 -0400

    Test and build player's health methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,6 @@
 const Player = require("../lib/Player");
 const Potion = require("../lib/Potion");
+
 jest.mock("../lib/Potion");
 
 console.log(new Potion());
@@ -11,6 +12,7 @@ test("creates a player object", () => {
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
+
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
@@ -33,4 +35,35 @@ test("gets inventory from player or returns false", () => {
   player.inventory = [];
 
   expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player's health value", () => {
+  const player = new Player("Dave");
+
+  expect(player.getHealth()).toEqual(
+    expect.stringContaining(player.health.toString())
+  );
+});
+
+test("checks if player is alive or not", () => {
+  const player = new Player("Dave");
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+  const player = new Player("Dave");
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
 });
